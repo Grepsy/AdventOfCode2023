77cd5eb3c1ea566658a3dd52944e90c6a0871556
Robert Massa <robertmassa@gmail.com>
2023-12-07 11:37:08 +0100

    Day 7 prep.

---
+++ Day7.cs
@@ -0,0 +1,8 @@
+﻿public static partial class Day7 {
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex DigitsRegex();
+
+    public static int Part1() => 0;
+
+    public static int Part2() => 0;
+}
